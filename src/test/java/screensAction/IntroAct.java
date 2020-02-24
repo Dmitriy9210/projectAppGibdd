e917753337a4195b4111b8dc2621dfa329fa5520
Dmitriy9210 <dimon9210@mail.ru>
2020-02-24 14:38:03 +0300

    рефакторинг тестов для задания по "Allure и его анотации"

--- src/test/java/screensAction/IntroAct.java
+++ src/test/java/screensAction/IntroAct.java
@@ -2,7 +2,7 @@ package screensAction;
 
 import com.sun.org.apache.xml.internal.utils.URI;
 import gibdd.TestGibdd;
-import io.qameta.allure.Description;
+import io.qameta.allure.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
@@ -15,11 +15,10 @@ public class IntroAct extends IntroSelectors {
 
     private TestGibdd gibdd = new TestGibdd();
 
-
     @BeforeMethod(groups = "search")
     public void setUp() throws URI.MalformedURIException, MalformedURLException {
         gibdd.setUp();
-        gibdd.waiter(By.xpath("//*[contains(@text, 'есть')]"), 50);
+        gibdd.waiter(By.xpath("//*[contains(@text, 'есть')]"), 90);
     }
 
     @AfterMethod(groups = "search")
@@ -32,31 +31,43 @@ public class IntroAct extends IntroSelectors {
         gibdd.closeApp();
     }
 
-    @Test (groups = "open")
+    @Owner(value = "Дмитрий Авраменко")
+    @Severity(value = SeverityLevel.CRITICAL)
+    @Story(value = "Страница Интро")
+    @Test(groups = "open")
     @Description(value = "Открытие приложения")
     public void openMenu() {
         WebElement buttonMenu = gibdd.waiter(By.xpath("//*[contains(@text, 'menu')]"), 20);
         buttonMenu.click();
     }
 
+    @Owner(value = "Дмитрий Авраменко")
+    @Severity(value = SeverityLevel.MINOR)
+    @Story(value = "Страница Интро")
     @Parameters("docSTS")
-    @Test (groups = "search")
+    @Test(groups = "search")
     @Description(value = "Заполнение полей: СТС")
-    public void searchSts(String docSTS)   {
+    public void searchSts(String docSTS) {
         WebElement inputSts = gibdd.waiter(By.xpath("//android.view.View[1]/android.view.View[1]/android.view.View/android.view.View[1]/android.view.View[2]/android.widget.EditText"), 20);
         inputSts.sendKeys(docSTS);
         gibdd.waiter(By.xpath("//*[contains(@text, 'НАЙТИ')]"), 3).click();
     }
 
+    @Owner(value = "Дмитрий Авраменко")
+    @Severity(value = SeverityLevel.NORMAL)
+    @Story(value = "Страница Интро")
     @Parameters("docVY")
-    @Test (groups = "search")
+    @Test(groups = "search")
     @Description(value = "Заполнение полей: ВУ")
-    public void searchVy(String docVY)   {
+    public void searchVy(String docVY) {
         WebElement inputVy = gibdd.waiter(By.xpath("//android.view.View[2]/android.view.View[1]/android.view.View/android.view.View[1]/android.view.View[2]/android.widget.EditText"), 20);
         inputVy.sendKeys(docVY);
         gibdd.waiter(By.xpath("//*[contains(@text, 'НАЙТИ')]"), 3).click();
     }
 
+    @Owner(value = "Дмитрий Авраменко")
+    @Severity(value = SeverityLevel.MINOR)
+    @Story(value = "Страница Интро")
     @Parameters({"docSTS", "docVY"})
     @Test(groups = "search")
     @Description(value = "Заполнение полей: СТС и ВУ")
@@ -68,15 +79,18 @@ public class IntroAct extends IntroSelectors {
         gibdd.waiter(By.xpath("//*[contains(@text, 'НАЙТИ')]"), 3).click();
     }
 
-    @Parameters("nomberUIN")
+    @Owner(value = "Дмитрий Авраменко")
+    @Severity(value = SeverityLevel.MINOR)
+    @Story(value = "Страница Интро")
+    @Parameters("numberUIN")
     @Test(groups = "searchUIN")
     @Description(value = "Заполнение полей: УИН")
-    public void searchUin(String nomberUIN) {
+    public void searchUin(String numberUIN) {
         WebElement findUINbtn = gibdd.waiter(By.xpath("//*[contains(@text, 'ОПЛАТИТЬ')]"), 20);
         findUINbtn.click();
-        gibdd.waiter(By.xpath("//*[contains(@text, 'ПО ПОСТА')]"),30);
+        gibdd.waiter(By.xpath("//*[contains(@text, 'ПО ПОСТА')]"), 30);
         WebElement findUIN = gibdd.waiter(By.xpath("//android.widget.EditText"), 40);
-        findUIN.sendKeys(nomberUIN);
+        findUIN.sendKeys(numberUIN);
         gibdd.waiter(By.xpath("//*[contains(@text, 'НАЙТИ')]"), 10).click();
     }
 
